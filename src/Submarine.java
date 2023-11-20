3527a1b515cfb0628010b212292a0ccdaaf12b28
Fátima <fatimaalvesa10@gmail.com>
2023-11-20 17:37:15 +0000

    colors2

--- src/Submarine.java
+++ src/Submarine.java
@@ -1,5 +1,5 @@
 public class Submarine extends Ship {
     public Submarine(String name, int size, String symbol) {
-        super(Submarine, 3, symbol);
+        super("Submarine", 3, symbol);
     }
 }
